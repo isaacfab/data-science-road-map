f57e04e55d277f19212ba071bf3d9b0297b87676
Isaac Faber <isaacfab@gmail.com>
2025-08-07 13:07:29 -0700

    fix: load model on import

--- 4-machine-learning-engineer/ml_python/app/app.py
+++ 4-machine-learning-engineer/ml_python/app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 
+# Load the model at import time so it's available for all requests
+rf = joblib.load('model.pkl')
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,11 +13,10 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
      json_ = request.get_json()
-     query_df = pd.json_normalize(json_)     
+     query_df = pd.json_normalize(json_)
      query = query_df.to_numpy()
      prediction = rf.predict(query)
-     return jsonify({'prediction':prediction.tolist()})
+     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
-    rf = joblib.load('model.pkl')
-    app.run(host = '0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
